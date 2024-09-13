0d26a91eade42e3a61438726dc0a2f51f821ddff
MartinBenicek <63211618+MartinBenicek@users.noreply.github.com>
2024-09-13 14:41:51 +0200

    navbar

--- app/(group)/layout.tsx
+++ app/(group)/layout.tsx
@@ -1,3 +1,4 @@
+import NavMenu from "@/components/NavMenu";
 import HeaderComponent from "../../components/HeaderComponent";
 
 export default function RootLayout({
@@ -6,23 +7,24 @@ export default function RootLayout({
     children: React.ReactNode;
   }>) {
     return (
-      <html lang="en">
-        <body className="flex flex-col min-h-screen">
-          <header className="flex items-center h-24 bg-gradient-to-r from-amber-800 via-amber-600 to-amber-800 justify-between">
-              <HeaderComponent url="/" image="home" text="Domů"></HeaderComponent>
-              <span className="flex justify-between mr-5">
+      <section className="flex flex-col min-h-screen">
+        <header className="flex items-center h-24 bg-gradient-to-r from-amber-800 via-amber-600 to-amber-800 justify-between">
+            <HeaderComponent url="/" image="home" text="Domů"></HeaderComponent>
+            <span className="flex justify-between mr-5">
+              <span className="hidden md:flex">
                 <HeaderComponent url="/uvod" image="uvod" text="Úvod"></HeaderComponent>
                 <HeaderComponent url="/pocitaniKaret" image="pocitadlo" text="Počítání Karet"></HeaderComponent>
                 <HeaderComponent url="/cena" image="penize" text="Cena"></HeaderComponent>
               </span>
-          </header>
-          <main className="flex-grow">
-          {children}
-          </main>
-          <footer className="bg-gradient-to-r from-amber-800 via-amber-600 to-amber-800 p-10">
-            <p>lmao</p>
-          </footer>
-        </body>
-      </html>
+              <NavMenu image="nav-closed" text="menu"></NavMenu>
+            </span>
+        </header>
+        <main className="flex-grow">
+        {children}
+        </main>
+        <footer className="bg-gradient-to-r from-amber-800 via-amber-600 to-amber-800 p-10">
+          <p>lmao</p>
+        </footer>
+      </section>
     );
   }
