2f3585eac48b816a1fc094b60f3b73795f1a0856
MartinBenicek <martabenic@seznam.cz>
2025-02-03 14:01:40 +0100

    sign in image fix

--- app/(group)/layout.tsx
+++ app/(group)/layout.tsx
@@ -42,7 +42,7 @@ export default async function RootLayout({
             ) : (
               <HeaderComponent
                 url="/signin"
-                image="SignIn"
+                image="signIn"
                 text="Přihlásit se"
               ></HeaderComponent>
             )}
